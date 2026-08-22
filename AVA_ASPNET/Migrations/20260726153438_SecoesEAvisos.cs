b09f05c3a1dc232a2574fbf28491cbd5e9cf8189
NDias <isadoranunesdias39@gmail.com>
2026-08-22 19:22:06 -0300

    Criar Secoes e Aviso

--- AVA_ASPNET/Migrations/20260726153438_SecoesEAvisos.cs
+++ AVA_ASPNET/Migrations/20260726153438_SecoesEAvisos.cs
@@ -10,13 +10,51 @@ namespace AVA_ASPNET.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Secoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(
+                        type: "int",
+                        nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
 
+                    Nome = table.Column<string>(
+                        type: "nvarchar(100)",
+                        maxLength: 100,
+                        nullable: false),
+
+                    Ordem = table.Column<int>(
+                        type: "int",
+                        nullable: false),
+
+                    TurmaId = table.Column<int>(
+                        type: "int",
+                        nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secoes", x => x.Id);
+
+                    table.ForeignKey(
+                        name: "FK_Secoes_Turmas_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "Turmas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Secoes_TurmaId",
+                table: "Secoes",
+                column: "TurmaId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "Secoes");
         }
     }
 }
